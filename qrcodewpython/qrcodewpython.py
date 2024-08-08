e37b0a151d53afc852af4e8800ee9dfbf2f746d9
selin pir <113732977+selinpir@users.noreply.github.com>
2024-08-08 23:31:41 +0300

    0808

---
+++ qrcodewpython/qrcodewpython.py
@@ -0,0 +1,23 @@
+
+"""
+qr kod oluşturma:
+
+    python -m
+    + pip install qrcode
+    + pip install image
+
+"""
+
+import qrcode
+import image
+qr=qrcode.QRCode(version=15,
+                 box_size=10,
+                 border=5)
+data="selinisme"
+
+qr.add_data(data)
+
+qr.make(fit=True)
+
+img = qr.make_image(fill="black",back_color="white")
+img.save("qr.png")
